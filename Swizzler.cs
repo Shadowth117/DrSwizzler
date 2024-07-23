8a8bc361fd9d3d97ae131cccdf99bcc4070d6739
Shadow <shadic@msn.com>
2024-07-23 00:47:30 -0400

    Add option for min size buffer completely and bump version again

--- Swizzler.cs
+++ Swizzler.cs
@@ -9,18 +9,18 @@ namespace DrSwizzler
         /// <summary>
         /// Massive credit to Agrajag for Vita Deswizzling
         /// </summary>
-        public static byte[] VitaSwizzle(byte[] swizzledData, int width, int height, DXGIFormat pixelFormat)
+        public static byte[] VitaSwizzle(byte[] swizzledData, int width, int height, DXGIFormat pixelFormat, int minBufferSize = 0x0)
         {
             GetsourceBytesPerPixelSetAndPixelSize(pixelFormat, out var sourceBytesPerPixelSet, out var pixelBlockSize, out int formatbpp);
-            return VitaSwizzle(swizzledData, width, height, sourceBytesPerPixelSet, formatbpp);
+            return VitaSwizzle(swizzledData, width, height, sourceBytesPerPixelSet, formatbpp, minBufferSize);
         }
 
         /// <summary>
         /// Massive credit to Agrajag for Vita Deswizzling
         /// </summary>
-        public static byte[] VitaSwizzle(byte[] swizzledData, int width, int height, int sourceBytesPerPixelSet, int formatbpp)
+        public static byte[] VitaSwizzle(byte[] swizzledData, int width, int height, int sourceBytesPerPixelSet, int formatbpp, int minBufferSize = 0x0)
         {
-            return VitaSwizzler.VitaSwizzle(swizzledData, width, height, sourceBytesPerPixelSet, formatbpp);
+            return VitaSwizzler.VitaSwizzle(swizzledData, width, height, sourceBytesPerPixelSet, formatbpp, minBufferSize);
         }
 
         public static byte[] Xbox360Swizzle(byte[] swizzledData, int width, int height, DXGIFormat pixelFormat)
@@ -37,57 +37,57 @@ namespace DrSwizzler
         /// <summary>
         /// Based on RawTex Implementation
         /// </summary>
-        public static byte[] PS3Swizzle(byte[] swizzledData, int width, int height, DXGIFormat pixelFormat)
+        public static byte[] PS3Swizzle(byte[] swizzledData, int width, int height, DXGIFormat pixelFormat, int minBufferSize = 0x0)
         {
             GetsourceBytesPerPixelSetAndPixelSize(pixelFormat, out var sourceBytesPerPixelSet, out var pixelBlockSize, out int formatbpp);
-            return PS3Swizzle(swizzledData, width, height, sourceBytesPerPixelSet, pixelBlockSize, formatbpp);
+            return PS3Swizzle(swizzledData, width, height, sourceBytesPerPixelSet, pixelBlockSize, formatbpp, minBufferSize);
         }
 
-        public static byte[] PS3Swizzle(byte[] swizzledData, int width, int height, int sourceBytesPerPixelSet, int pixelBlockSize, int formatbpp)
+        public static byte[] PS3Swizzle(byte[] swizzledData, int width, int height, int sourceBytesPerPixelSet, int pixelBlockSize, int formatbpp, int minBufferSize = 0x0)
         {
-            return PS3Swizzler.PS3Swizzle(swizzledData, width, height, sourceBytesPerPixelSet, pixelBlockSize, formatbpp);
+            return PS3Swizzler.PS3Swizzle(swizzledData, width, height, sourceBytesPerPixelSet, pixelBlockSize, formatbpp, minBufferSize);
         }
 
         /// <summary>
         /// Based on RawTex Implementation
         /// </summary>
-        public static byte[] PS4Swizzle(byte[] swizzledData, int width, int height, DXGIFormat pixelFormat)
+        public static byte[] PS4Swizzle(byte[] swizzledData, int width, int height, DXGIFormat pixelFormat, int minBufferSize = 0x200)
         {
             GetsourceBytesPerPixelSetAndPixelSize(pixelFormat, out var sourceBytesPerPixelSet, out var pixelBlockSize, out int formatbpp);
-            return PS4Swizzle(swizzledData, width, height, sourceBytesPerPixelSet, pixelBlockSize, formatbpp);
+            return PS4Swizzle(swizzledData, width, height, sourceBytesPerPixelSet, pixelBlockSize, formatbpp, minBufferSize);
         }
 
-        public static byte[] PS4Swizzle(byte[] swizzledData, int width, int height, int sourceBytesPerPixelSet, int pixelBlockSize, int formatbpp)
+        public static byte[] PS4Swizzle(byte[] swizzledData, int width, int height, int sourceBytesPerPixelSet, int pixelBlockSize, int formatbpp, int minBufferSize = 0x200)
         {
-            return PS4Swizzler.PS4Swizzle(swizzledData, width, height, sourceBytesPerPixelSet, pixelBlockSize, formatbpp);
+            return PS4Swizzler.PS4Swizzle(swizzledData, width, height, sourceBytesPerPixelSet, pixelBlockSize, formatbpp, minBufferSize);
         }
 
         /// <summary>
         /// Based on RawTex Implementation
         /// </summary>
-        public static byte[] PS5Swizzle(byte[] swizzledData, int width, int height, DXGIFormat pixelFormat)
+        public static byte[] PS5Swizzle(byte[] swizzledData, int width, int height, DXGIFormat pixelFormat, int minBufferSize = 0x100)
         {
             GetsourceBytesPerPixelSetAndPixelSize(pixelFormat, out var sourceBytesPerPixelSet, out var pixelBlockSize, out int formatbpp);
-            return PS5Swizzle(swizzledData, width, height, sourceBytesPerPixelSet, pixelBlockSize, formatbpp);
+            return PS5Swizzle(swizzledData, width, height, sourceBytesPerPixelSet, pixelBlockSize, formatbpp, minBufferSize);
         }
 
-        public static byte[] PS5Swizzle(byte[] swizzledData, int width, int height, int sourceBytesPerPixelSet, int pixelBlockSize, int formatbpp)
+        public static byte[] PS5Swizzle(byte[] swizzledData, int width, int height, int sourceBytesPerPixelSet, int pixelBlockSize, int formatbpp, int minBufferSize = 0x100)
         {
-            return PS5Swizzler.PS5Swizzle(swizzledData, width, height, sourceBytesPerPixelSet, pixelBlockSize, formatbpp);
+            return PS5Swizzler.PS5Swizzle(swizzledData, width, height, sourceBytesPerPixelSet, pixelBlockSize, formatbpp, minBufferSize);
         }
 
         /// <summary>
         /// Based on RawTex Implementation
         /// </summary>
-        public static byte[] SwitchSwizzle(byte[] swizzledData, int width, int height, DXGIFormat pixelFormat)
+        public static byte[] SwitchSwizzle(byte[] swizzledData, int width, int height, DXGIFormat pixelFormat, int minBufferSize = 0x0)
         {
             GetsourceBytesPerPixelSetAndPixelSize(pixelFormat, out var sourceBytesPerPixelSet, out var pixelBlockSize, out int formatbpp);
-            return SwitchSwizzle(swizzledData, width, height, sourceBytesPerPixelSet, pixelBlockSize, formatbpp);
+            return SwitchSwizzle(swizzledData, width, height, sourceBytesPerPixelSet, pixelBlockSize, formatbpp, minBufferSize);
         }
 
-        public static byte[] SwitchSwizzle(byte[] swizzledData, int width, int height, int sourceBytesPerPixelSet, int pixelBlockSize, int formatbpp)
+        public static byte[] SwitchSwizzle(byte[] swizzledData, int width, int height, int sourceBytesPerPixelSet, int pixelBlockSize, int formatbpp, int minBufferSize = 0x0)
         {
-            return SwitchSwizzler.SwitchSwizzle(swizzledData, width, height, sourceBytesPerPixelSet, pixelBlockSize, formatbpp);
+            return SwitchSwizzler.SwitchSwizzle(swizzledData, width, height, sourceBytesPerPixelSet, pixelBlockSize, formatbpp, minBufferSize);
         }
     }
 }
