1421f96c1a44ce18ebb081810dc938089efcee00
DarkShadow <ivanivov.p@gmail.com>
2025-03-02 23:43:48 +0200

    Added BitsPerPixel and IsPixelFormatCompressed helper functions

--- Util.cs
+++ Util.cs
@@ -6,127 +6,186 @@ namespace DrSwizzler
     public class Util
     {
         /// <summary>
-        /// RawTex Implementation
+        /// Based on DirectXTex
         /// </summary>
-        public static int[] bitsPerPixel = new int[116]
+        public static int BitsPerPixel(DXGIFormat pixelFormat)
         {
-              0,
-              128,
-              128,
-              128,
-              128,
-              96,
-              96,
-              96,
-              96,
-              64,
-              64,
-              64,
-              64,
-              64,
-              64,
-              64,
-              64,
-              64,
-              64,
-              64,
-              64,
-              64,
-              64,
-              32,
-              32,
-              32,
-              32,
-              32,
-              32,
-              32,
-              32,
-              32,
-              32,
-              32,
-              32,
-              32,
-              32,
-              32,
-              32,
-              32,
-              32,
-              32,
-              32,
-              32,
-              32,
-              32,
-              32,
-              32,
-              16,
-              16,
-              16,
-              16,
-              16,
-              16,
-              16,
-              16,
-              16,
-              16,
-              16,
-              16,
-              8,
-              8,
-              8,
-              8,
-              8,
-              8,
-              1,
-              32,
-              32,
-              32,
-              4,
-              4,
-              4,
-              8,
-              8,
-              8,
-              8,
-              8,
-              8,
-              4,
-              4,
-              4,
-              8,
-              8,
-              8,
-              16,
-              16,
-              32,
-              32,
-              32,
-              32,
-              32,
-              32,
-              32,
-              8,
-              8,
-              8,
-              8,
-              8,
-              8,
-              0,
-              0,
-              0,
-              0,
-              0,
-              0,
-              0,
-              0,
-              0,
-              0,
-              0,
-              0,
-              0,
-              0,
-              0,
-              16
-        };
+            switch (pixelFormat)
+            {
+                case DXGIFormat.R32G32B32A32TYPELESS:
+                case DXGIFormat.R32G32B32A32FLOAT:
+                case DXGIFormat.R32G32B32A32UINT:
+                case DXGIFormat.R32G32B32A32SINT:
+                    return 128;
+
+                case DXGIFormat.R32G32B32TYPELESS:
+                case DXGIFormat.R32G32B32FLOAT:
+                case DXGIFormat.R32G32B32UINT:
+                case DXGIFormat.R32G32B32SINT:
+                    return 96;
+
+                case DXGIFormat.R16G16B16A16TYPELESS:
+                case DXGIFormat.R16G16B16A16FLOAT:
+                case DXGIFormat.R16G16B16A16UNORM:
+                case DXGIFormat.R16G16B16A16UINT:
+                case DXGIFormat.R16G16B16A16SNORM:
+                case DXGIFormat.R16G16B16A16SINT:
+                case DXGIFormat.R32G32TYPELESS:
+                case DXGIFormat.R32G32FLOAT:
+                case DXGIFormat.R32G32UINT:
+                case DXGIFormat.R32G32SINT:
+                case DXGIFormat.R32G8X24TYPELESS:
+                case DXGIFormat.D32FLOATS8X24UINT:
+                case DXGIFormat.R32FLOATX8X24TYPELESS:
+                case DXGIFormat.X32TYPELESSG8X24UINT:
+                case DXGIFormat.Y416:
+                case DXGIFormat.Y210:
+                case DXGIFormat.Y216:
+                    return 64;
+
+                case DXGIFormat.R10G10B10A2TYPELESS:
+                case DXGIFormat.R10G10B10A2UNORM:
+                case DXGIFormat.R10G10B10A2UINT:
+                case DXGIFormat.R11G11B10FLOAT:
+                case DXGIFormat.R8G8B8A8TYPELESS:
+                case DXGIFormat.R8G8B8A8UNORM:
+                case DXGIFormat.R8G8B8A8UNORMSRGB:
+                case DXGIFormat.R8G8B8A8UINT:
+                case DXGIFormat.R8G8B8A8SNORM:
+                case DXGIFormat.R8G8B8A8SINT:
+                case DXGIFormat.R16G16TYPELESS:
+                case DXGIFormat.R16G16FLOAT:
+                case DXGIFormat.R16G16UNORM:
+                case DXGIFormat.R16G16UINT:
+                case DXGIFormat.R16G16SNORM:
+                case DXGIFormat.R16G16SINT:
+                case DXGIFormat.R32TYPELESS:
+                case DXGIFormat.D32FLOAT:
+                case DXGIFormat.R32FLOAT:
+                case DXGIFormat.R32UINT:
+                case DXGIFormat.R32SINT:
+                case DXGIFormat.R24G8TYPELESS:
+                case DXGIFormat.D24UNORMS8UINT:
+                case DXGIFormat.R24UNORMX8TYPELESS:
+                case DXGIFormat.X24TYPELESSG8UINT:
+                case DXGIFormat.R9G9B9E5SHAREDEXP:
+                case DXGIFormat.R8G8B8G8UNORM:
+                case DXGIFormat.G8R8G8B8UNORM:
+                case DXGIFormat.B8G8R8A8UNORM:
+                case DXGIFormat.B8G8R8X8UNORM:
+                case DXGIFormat.R10G10B10XRBIASA2UNORM:
+                case DXGIFormat.B8G8R8A8TYPELESS:
+                case DXGIFormat.B8G8R8A8UNORMSRGB:
+                case DXGIFormat.B8G8R8X8TYPELESS:
+                case DXGIFormat.B8G8R8X8UNORMSRGB:
+                case DXGIFormat.AYUV:
+                case DXGIFormat.Y410:
+                case DXGIFormat.YUY2:
+                    return 32;
+
+                case DXGIFormat.P010:
+                case DXGIFormat.P016:
+                    return 24;
+
+                case DXGIFormat.R8G8TYPELESS:
+                case DXGIFormat.R8G8UNORM:
+                case DXGIFormat.R8G8UINT:
+                case DXGIFormat.R8G8SNORM:
+                case DXGIFormat.R8G8SINT:
+                case DXGIFormat.R16TYPELESS:
+                case DXGIFormat.R16FLOAT:
+                case DXGIFormat.D16UNORM:
+                case DXGIFormat.R16UNORM:
+                case DXGIFormat.R16UINT:
+                case DXGIFormat.R16SNORM:
+                case DXGIFormat.R16SINT:
+                case DXGIFormat.B5G6R5UNORM:
+                case DXGIFormat.B5G5R5A1UNORM:
+                case DXGIFormat.A8P8:
+                case DXGIFormat.B4G4R4A4UNORM:
+                case DXGIFormat.A4B4G4R4UNORM:
+                    return 16;
+
+                case DXGIFormat.NV12:
+                case DXGIFormat.OPAQUE420:
+                case DXGIFormat.NV11:
+                    return 12;
+
+                case DXGIFormat.R8TYPELESS:
+                case DXGIFormat.R8UNORM:
+                case DXGIFormat.R8UINT:
+                case DXGIFormat.R8SNORM:
+                case DXGIFormat.R8SINT:
+                case DXGIFormat.A8UNORM:
+                case DXGIFormat.BC2TYPELESS:
+                case DXGIFormat.BC2UNORM:
+                case DXGIFormat.BC2UNORMSRGB:
+                case DXGIFormat.BC3TYPELESS:
+                case DXGIFormat.BC3UNORM:
+                case DXGIFormat.BC3UNORMSRGB:
+                case DXGIFormat.BC5TYPELESS:
+                case DXGIFormat.BC5UNORM:
+                case DXGIFormat.BC5SNORM:
+                case DXGIFormat.BC6HTYPELESS:
+                case DXGIFormat.BC6HUF16:
+                case DXGIFormat.BC6HSF16:
+                case DXGIFormat.BC7TYPELESS:
+                case DXGIFormat.BC7UNORM:
+                case DXGIFormat.BC7UNORMSRGB:
+                case DXGIFormat.AI44:
+                case DXGIFormat.IA44:
+                case DXGIFormat.P8:
+                    return 8;
+
+                case DXGIFormat.R1UNORM:
+                    return 1;
+
+                case DXGIFormat.BC1TYPELESS:
+                case DXGIFormat.BC1UNORM:
+                case DXGIFormat.BC1UNORMSRGB:
+                case DXGIFormat.BC4TYPELESS:
+                case DXGIFormat.BC4UNORM:
+                case DXGIFormat.BC4SNORM:
+                    return 4;
+
+                default:
+                    return 0;
+            }
+        }
+
+        /// <summary>
+        /// Based on DirectXTex
+        /// </summary>
+        public static bool IsPixelFormatCompressed(DXGIFormat pixelFormat)
+        {
+            switch (pixelFormat)
+            {
+                case DXGIFormat.BC1TYPELESS:
+                case DXGIFormat.BC1UNORM:
+                case DXGIFormat.BC1UNORMSRGB:
+                case DXGIFormat.BC2TYPELESS:
+                case DXGIFormat.BC2UNORM:
+                case DXGIFormat.BC2UNORMSRGB:
+                case DXGIFormat.BC3TYPELESS:
+                case DXGIFormat.BC3UNORM:
+                case DXGIFormat.BC3UNORMSRGB:
+                case DXGIFormat.BC4TYPELESS:
+                case DXGIFormat.BC4UNORM:
+                case DXGIFormat.BC4SNORM:
+                case DXGIFormat.BC5TYPELESS:
+                case DXGIFormat.BC5UNORM:
+                case DXGIFormat.BC5SNORM:
+                case DXGIFormat.BC6HTYPELESS:
+                case DXGIFormat.BC6HUF16:
+                case DXGIFormat.BC6HSF16:
+                case DXGIFormat.BC7TYPELESS:
+                case DXGIFormat.BC7UNORM:
+                case DXGIFormat.BC7UNORMSRGB:
+                    return true;
+                default:
+                    return false;
+            }
+        }
 
         /// <summary>
         /// RawTex Switch Texture Info
@@ -167,14 +226,15 @@ namespace DrSwizzler
               31
         };
 
+        
+
         /// <summary>
         /// Based on RawTex handling
         /// </summary>
         public static void GetsourceBytesPerPixelSetAndPixelSize(DXGIFormat pixelFormat, out int sourceBytesPerPixelSet, out int pixelBlockSize, out int formatBpp)
         {
-            int pixelFormatInt = (int)pixelFormat;
             //The amount of pixels chunked together in a particular 'block'. Formats such as the DXT formats lump multiple pixels into one 'block' and must be handled as such.
-            if ((pixelFormatInt >= 70 && pixelFormatInt <= 84) || (pixelFormatInt >= 94 && pixelFormatInt <= 99))
+            if (IsPixelFormatCompressed(pixelFormat))
             {
                 pixelBlockSize = 4;
             }
@@ -183,7 +243,7 @@ namespace DrSwizzler
                 pixelBlockSize = 1;
             }
 
-            formatBpp = bitsPerPixel[pixelFormatInt];
+            formatBpp = BitsPerPixel(pixelFormat);
             if (pixelBlockSize == 1)
             {
                 sourceBytesPerPixelSet = formatBpp / 8;
